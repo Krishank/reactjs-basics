8b4e0c8565e6df1daa93f524ffd54d5a3bc450f6
Krishank Dwivedi <kdwivedi2@sapient.com>
2018-12-25 14:17:42 +0000

    Render Multiple components

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,27 @@
-import React from "react"
-import { render } from "react-dom"
+import React from 'react';
+import { render } from 'react-dom';
+
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
 
 class App extends React.Component {
-  render(){
-    return(
-      <h1>Hello React First Component</h1>
-    )
-  }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Header/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Home/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
-render(<App/>, window.document.getElementById('app'));
+
+render(<App />, window.document.getElementById('app'));
